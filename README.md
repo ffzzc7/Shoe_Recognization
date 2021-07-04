8cf4872e06a72c00fba894fc1815a2b4dd0c20ae
wangyazhoubjut <2250828550@qq.com>
2021-07-04 13:55:36 +0800

    add server code
    add server code

--- README.md
+++ README.md
@@ -11,3 +11,10 @@
 （3）打开index.js文件，将wx.uploadFile接口中的url参数内的ip换成服务端ip
 （4）编译
 （5）通过手机微信真机调试，点击小程序中间的上传图片按钮，拍照上传或者上传本地图片；正常情况下，图片所属的类别的序号通过弹框的方式显示在小程序界面上。
+
+
+3:服务端工程创建
+  （1）使用cmake管理整个工程，需要在IDE（Qt、Clion）或者其他支持打开cmake pro的IDE。
+  （2）开源drogon的server库需要依赖一些库（json uuid等），请自行按照drogon开源github进行依赖库的安装。（按照教程安装即可）
+  （3） 项目中使用了opencv库以及libtorch库。将在文中附加。
+  （4）工程打开后 编译运行 将在本机发布 url 192.168.2.105/shoesRecognization/Intetface/shoesrecognization/request，客户端通过post方式像此链接发送图片即可。其中ip是server端运行ip地址。
--- code/server/shoesRecognization_Intetface_shoesrecognization.cc
+++ code/server/shoesRecognization_Intetface_shoesrecognization.cc
@@ -1,24 +1,100 @@
 #include "shoesRecognization_Intetface_shoesrecognization.h"
-using namespace shoesRecognization::Intetface;
-//add definition of your processing function here
 
-
-shoesrecognization::shoesrecognization() {}
-
-void shoesrecognization::receiveRequest(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback) const
+namespace shoesRecognization
 {
-
-
-    MultiPartParser fileUpload;
-    if (fileUpload.parse(req) == 0)
+    namespace Intetface
     {
+        shoesrecognization::shoesrecognization() {}
+
+        void shoesrecognization::receiveRequest(const HttpRequestPtr &req,std::function<void(const HttpResponsePtr &)> &&callback) const
+        {
+            LOG_DEBUG << "recive wx !";
+            MultiPartParser fileUpload;
+            if (fileUpload.parse(req) == 0)
+            {
+                //LOG_DEBUG << "upload !";
+                Json::Value rootJson;  //返回的json数据包
+
+                auto parameters = fileUpload.getParameters();
+                std::string PicID = parameters["PicID"];
+                auto &files = fileUpload.getFiles();
+                for (auto const &file : files) {
+
+                    //LOG_DEBUG << "PicID !";
+
+                    std::string fileName = file.getFileName();
+                    file.save();
+                    using torch::jit::script::Module;
+                    Module module = torch::jit::load("MobileNetV3_CPU.pt");
+                    module.to(at::kCPU);
+                    LOG_DEBUG << ("fileName %s!",fileName);
+                    std::string strLocalPath =app().getUploadPath();
+                    std::string strLocalRecImage =strLocalPath+'/'+fileName;
+
+                    LOG_DEBUG << strLocalRecImage;
+                    auto image = cv::imread(strLocalRecImage, cv::ImreadModes::IMREAD_COLOR);
+                     //cv::Mat image(file.fileLength(),CV_8UC1,(char*)file.fileData());
+
+                    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+                    cv::Mat img_float;
+                    image.convertTo(img_float, CV_32F, 1.0 / 255);
+                    // 3. resize the image for resnet101 model
+                    cv::resize(img_float, img_float, cv::Size(224, 224), cv::INTER_AREA);
+                    // 4. transform to tensor
+                    auto img_tensor = torch::from_blob(img_float.data, {1, 224, 224, 3}, torch::kFloat32);
+                    // in pytorch, batch first, then channel
+                    img_tensor = img_tensor.permute({0, 3, 1, 2});
+                    // 5. Removing mean values of the RGB channels
+                    // the values are from following link.
+                    img_tensor[0][0] = img_tensor[0][0].sub_(0.485).div_(0.229);
+                    img_tensor[0][1] = img_tensor[0][1].sub_(0.456).div_(0.224);
+                    img_tensor[0][2] = img_tensor[0][2].sub_(0.406).div_(0.225);
+
+                    img_tensor = img_tensor.to(at::kCPU);
+                    // 网络前向计算
+                    at::Tensor output = module.forward({img_tensor}).toTensor();
+                    //std::cout << "output:" << output << std::endl;
+
+                    auto prediction = output.argmax(1);
+                    std::cout << "prediction:" << prediction << std::endl;
+
+                    int maxk = 3;
+                    auto top3 = std::get<1>(output.topk(maxk, 1, true, true));
+
+                    std::cout << "top3: " << top3 << '\n';
+
+                    std::vector<int> res;
+                    for (auto i = 0; i < maxk; i++) {
+                        res.push_back(top3[0][i].item().toInt());
+                    }
+                    for (auto i : res) {
+                        std::cout << i << " ";
+                    }
+                    std::cout << "\n";
+
+
+                    auto max_result = output.max(1, true);
+                    auto max_index = std::get<1>(max_result).item<float>();
+                    std::cout << max_index << std::endl;
+
+                    LOG_DEBUG << max_index;
+
+                    Json::Value structJson;
+                    std::string strKey ="max_index";
+                    structJson[strKey] = max_index;
+
+                    rootJson.append(structJson);
+                    auto resp = HttpResponse::newHttpJsonResponse(structJson);
+                    callback(resp);
+                    return;
+                }
+            }
+
+            LOG_DEBUG << "upload error!";
+            Json::Value json;
+            json["result"] = "top3";
+            auto resp = HttpResponse::newHttpJsonResponse(json);
+            callback(resp);
+        }
     }
-    LOG_DEBUG << "upload error!";
-    Json::Value json;
-    json["result"] = "failed";
-    auto resp = HttpResponse::newHttpJsonResponse(json);
-    callback(resp);
-
-
-
 }
